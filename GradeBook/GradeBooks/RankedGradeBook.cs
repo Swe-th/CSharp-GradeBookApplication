af0798274d0ddc947bdc7ab815c6b71cf062afd6
Swethayn <swethayn@outlook.com>
2018-07-18 14:19:40 -0700

    Added override methods

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -4,9 +4,9 @@ using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
-    public class RankedGradeBook:BaseGradeBook
+    public class RankedGradeBook : BaseGradeBook
     {
-        public RankedGradeBook(string name):base(name)
+        public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
         }
@@ -15,7 +15,7 @@ namespace GradeBook.GradeBooks
 
             if (Students.Count < 5)
                 throw new System.InvalidOperationException("Ranked grading requires at least 5 students.");
-            var threshold=(int)Math.Ceiling(Students.Count*0.2);
+            var threshold = (int)Math.Ceiling(Students.Count * 0.2);
             var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
 
             if (grades[threshold - 1] <= averageGrade)
@@ -29,5 +29,26 @@ namespace GradeBook.GradeBooks
             else
                 return 'F';
         }
+        public override void CalculateStatistics()
+        {
+
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+        public override void CalculateStudentStatistics(string name)
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students.");
+                return;
+            }
+            base.CalculateStudentStatistics(name);
+
+        }
     }
 }
+
